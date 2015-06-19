efbd8f439f215b58b51cfc323bfd42968b327f48
Reed Mangino <reed@themanginos.com>
2015-06-19 11:31:37 -0400

    Finished using faker exercises

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,36 @@ require 'faker'
 50.times do
   Post.create!(title:  Faker::Lorem.sentence, body:   Faker::Lorem.paragraph)
 end
+
+# Create a unique post
+unique_post = Post.new(title: "A unique title", body: "A unique post body")
+post = Post.find_by(title: unique_post.title)
+if !post
+  # the post doesn't already exist so save it to the db
+  unique_post.save
+  puts "Unique post saved"
+  post = unique_post
+else
+  puts "Unique post already exists"
+end
+
 posts = Post.all
 
 # Create Comments
 100.times do
   Comment.create!(post: posts.sample, body: Faker::Lorem.paragraph)
+end
+
+# Create a unique comment on post
+unique_comment = Comment.new(post: post, body: "A unique comment body")
+comment = Comment.find_by(body: unique_comment.body)
+if !comment
+  # the comment doesn't already exist so save it to the db
+  unique_comment.save
+  puts "Unique comment saved"
+  comment = unique_comment
+else
+  puts "Unique comment already exists"
 end
 
 puts "Seed finished"
