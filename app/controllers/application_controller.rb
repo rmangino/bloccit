0e7af682ce2cd99d2e0dfd7dd213f2b6716a4e16
Reed Mangino <reed@themanginos.com>
2015-06-22 10:00:31 -0400

    Fixed a typo in application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  before_action: :configure_permitted_devise_parameters, if: :devise_controller?
+  before_action :configure_devise_permitted_parameters, if: :devise_controller?
 
 protected
 
   # Add name to the list of parameters accepted by devise during sign up.
-  def configure_permitted_devise_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
-  end
+  def configure_devise_permitted_parameters
+     devise_parameter_sanitizer.for(:sign_up) << :name
+   end
 end
