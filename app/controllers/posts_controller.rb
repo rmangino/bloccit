8bc13e66126afec630443466ba2f77a1a8ec1c75
Reed Mangino <reed@themanginos.com>
2015-06-23 08:05:55 -0400

    Completed authorization with Pundit exercises

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
-    authorize @posts
+    @posts = policy_scope(Post)
+    #@posts = Post.all
+    #authorize @posts
   end
 
   def show
@@ -10,14 +11,16 @@ class PostsController < ApplicationController
 
   # Create an empty post
   def new
-    @post = Post.new
-    authorize @post
+    @post = policy_scope(Post.new)
+    #@post = Post.new
+    #authorize @post
   end
 
   # Try to save a new post
   def create
-    @post = current_user.posts.build(params.require(:post).permit(:title, :body))
-    authorize @post
+    @post = policy_scope(current_user.posts.build(params.require(:post).permit(:title, :body)))
+    #@post = current_user.posts.build(params.require(:post).permit(:title, :body))
+    #authorize @post
     if @post.save
       flash[:notice] = "Post was saved."
       redirect_to @post
@@ -28,13 +31,15 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
-    authorize @post
+    @post = policy_scope(Post.find(params[:id]))
+    #@post = Post.find(params[:id])
+    #authorize @post
   end
 
   def update
-    @post = Post.find(params[:id])
-    authorize @post
+    @post = policy_scope(Post.find(params[:id]))
+    #@post = Post.find(params[:id])
+    #authorize @post
     if @post.update_attributes(params.require(:post).permit(:title, :body))
       flash[:notice] = "Post was updated."
       redirect_to @post
--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -3,4 +3,16 @@ class PostPolicy < ApplicationPolicy
   def index?
     true
   end
+
+  class Scope < Scope
+    def resolve
+      if user.admin? || user.moderator?
+        scope.all
+      elsif user.member?
+        scope.where(user_id: user.id)
+      else
+        [] # Any other roles see nothing
+      end
+    end
+  end
 end
